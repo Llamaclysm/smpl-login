10ca4266e89c8b121722d6c312d00ffdd48b33b5
Llamaclysm <t.wycinka+git@gmail.com>
2019-10-22 13:41:53 +0200

    register & login sections

--- auth_app.py
+++ auth_app.py
@@ -1,14 +1,22 @@
 from tkinter import *
 
+def register():
+    print("registered!")
+
+
+def login():
+    print("login!")
+
+
 def default_screen():
     screen = Tk()
     screen.geometry("350x200")
     screen.title("Auth App 0.1")
-    Label(text = "Auth App 0.1", bg = "grey", font = ("Calibri", 13)).pack()
+    Label(text = "Auth App 0.1", font = ("Calibri", 14)).pack()
     Label(text = "").pack()
-    Button(text = "Login").pack()
+    Button(text = "Login", command = login()).pack()
     Label(text="").pack()
-    Button(text = "Register").pack()
+    Button(text = "Register", command = register()).pack()
 
     screen.mainloop()
 
